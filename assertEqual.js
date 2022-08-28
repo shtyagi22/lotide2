7e49bb4c21bec5e06b03081aee099e46a4f950e8
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-28 13:20:10 -0400

    implemented assert equal function with test cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,10 @@
-const assertEqual = function(actual, expected) {
-
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
 };
 
 // TEST CODE
