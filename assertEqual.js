c42cd2627f1eeace14d213a619cc7f256c6837ec
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-28 13:31:08 -0400

    fixed linting

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,8 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }
-  else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
