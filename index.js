e5d357dce8bdfa090927a86615c577d19bda15ed
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-30 08:51:01 -0400

    exporting modules from index.js file

---
+++ index.js
@@ -0,0 +1,31 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const eqObjects = require('./eqObjects')
+const eqArrays = require('./eqArrays')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  takeUntil: takeUntil,
+  without: without
+};
