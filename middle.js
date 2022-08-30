dcc1c2c6792239c9aebe33a2d64961888bcf7011
shtyagi22 <sh.tyagi22@gmail.com>
2022-08-29 20:53:06 -0400

    refactoring with require and export module

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-
-  }
-
-
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function(array) {
   const result = [];
@@ -38,12 +14,6 @@ const middle = function(array) {
 
   }
 };
-//Test Code
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => [])
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5]));// => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
+module.exports = middle;
 
